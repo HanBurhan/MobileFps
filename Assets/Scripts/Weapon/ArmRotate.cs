c660f2eabd2c4eb20264ec906fcdc59607f491b9
Burhan <ozdemirburhan787@gmail.com>
2021-09-19 20:19:38 +0300

    Smooth Looking

---
+++ Assets/Scripts/Weapon/ArmRotate.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ArmRotate : MonoBehaviour
+{
+    [SerializeField] private float turnSmooth;
+
+    Transform cameraPos;
+
+    void Start()
+    {
+        cameraPos = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        float xAxis = cameraPos.transform.eulerAngles.x;
+        float yAxis = cameraPos.transform.eulerAngles.y;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(xAxis, yAxis, 0), turnSmooth * Time.deltaTime);
+    }
+}
